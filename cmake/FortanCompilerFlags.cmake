d6a96a848e8a2b7dd5f52b833090bb5aa7022e67
Guillaume Fraux <guillaume.fraux@chimie-paristech.fr>
2017-11-03 16:21:30 +0000

    Allow to build the code with intel or PGI compilers

--- cmake/FortanCompilerFlags.cmake
+++ cmake/FortanCompilerFlags.cmake
@@ -15,8 +15,18 @@ set_fortran_debug_flag_if_possible("-Wimplicit-interface")
 set_fortran_debug_flag_if_possible("-Wimplicit-procedure")
 set_fortran_debug_flag_if_possible("-Wno-c-binding-type")
 
+# intel ifort
+set_fortran_debug_flag_if_possible("-warn all")
+
 if(CMAKE_Fortran_COMPILER_ID MATCHES "GNU")
     set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -std=f2008 -pedantic")
     set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -ffree-form")
     set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -ffree-line-length-none")
+elseif(CMAKE_Fortran_COMPILER_ID MATCHES "Intel")
+    set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -stand f08 -warn stderrors")
+    set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -free")
+    set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -diag-disable 5268")
+elseif(CMAKE_Fortran_COMPILER_ID MATCHES "PGI")
+    set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -Mstandard -Mallocatable=03")
+    set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -Mfree")
 endif()
